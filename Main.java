7d1bdcfb2a5ad0cc401f64b7b5adbf3d98d60d55
GabrielHugo <hugogabriel1412@hotmail.com>
2025-03-26 23:36:11 +0100

    user response completed

--- Main.java
+++ Main.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         
-        System.out.println("Game");
+        Scanner firstQuestion = new Scanner(System.in); 
+        System.out.println("Choisis entre pierre, papier et ciseaux");
+
+        String userResponse = firstQuestion.nextLine();
+
+        //condition
+
+        if (userResponse.equals("pierre") || userResponse.equals("papier") || userResponse.equals("ciseaux")) {
+
+            System.out.println("Tu as choisis" + ' ' + userResponse);
+
+        } else {
+
+            System.err.println("error");
+            main(args);
+
+        }
 
     }
     
